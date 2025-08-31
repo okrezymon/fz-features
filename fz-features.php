f499ae9eeec0da3a98d7e4a90aa6536b7da6505a
olgakrezymon <olga@igamingnuts.com>
2025-08-31 18:55:30 +0200

    include faq block

--- fz-features.php
+++ fz-features.php
@@ -126,6 +126,13 @@ if ( !function_exists('fz_features_create_dummy_books' ) ) {
 }
 add_action( 'init', 'fz_features_create_dummy_books', 25 );
 
+// load functions
 require_once FZ_FEATURES_PATH . 'includes/functions/assets.php';
 require_once FZ_FEATURES_PATH . 'includes/functions/ajax.php';
+
+// load cpt, tax
 require_once FZ_FEATURES_PATH . 'includes/custom-post-types/fz-book.php';
+
+// load blocks
+require_once FZ_FEATURES_PATH . 'includes/blocks/fz-faq.php';
+
